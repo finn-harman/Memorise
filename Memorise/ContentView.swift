07e0a12908c0788b954e14bdb89976d088b114b8
FinnHarman <fh219@ic.ac.uk>
2020-10-27 17:48:01 +0000

    Started Development

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -9,10 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return Text("Hello There, world!")
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
