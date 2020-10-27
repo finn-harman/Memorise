6e5a7c22b16534c2417f5b52cedfaecaa6aecc37
FinnHarman <fh219@ic.ac.uk>
2020-10-27 18:29:37 +0000

    L1 - Introduction to SwiftUI

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        return Text("Hello There, world!")
+    var body: some View  {
+         HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
 
 
 
